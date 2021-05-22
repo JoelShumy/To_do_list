fa825fe929b9fabd19a2b2d86eb1ea8957ae6bfb
joel shum <joel.shumkwoong@gmail.com>
2021-05-22 14:58:52 +0200

    interface du projet

---
+++ script.js
@@ -0,0 +1,5 @@
+let userTask = document.getElementById("task_name");
+let submitButton = document.getElementById("submit_button");
+let taskList = document.getElementById("task_list");
+
+taskList.appendChild("<li>lol</li>");
