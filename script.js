4aeb9a8966aa92d7a3ab1b789902b6d5475de2cd
joel shum <joel.shumkwoong@gmail.com>
2021-05-23 18:26:38 +0200

    fonction effacer liste + ajout des boutons Done et Delete

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ let userTask = document.getElementById('task_name');
 let taskList = document.getElementById('tasks__list');
 let tasks = document.getElementsByTagName('li');
 
+
+
+function creatTaskButtons(list) {
+    let btnDone = document.createElement("BUTTON");
+    btnDone.innerHTML = "Done";
+    list.appendChild(btnDone);
+
+    let btnDelete = document.createElement("BUTTON");
+    btnDelete.innerHTML = "Delete";
+    list.appendChild(btnDelete);
+  }
+
+
+
 function createTask () {
     let taskValue = document.getElementById('task_name').value;
     let newTask = document.createElement('li');
+
+    if (taskValue.length >= 2) {
     newTask.innerText = taskValue;
-    
-taskList.appendChild(newTask)
+    creatTaskButtons(newTask);
+    taskList.appendChild(newTask)
+    } else {
+        alert("The task must be 2 caracters minimum");
+    }
 }
 
 
@@ -16,8 +35,10 @@ let clearButton =  document.getElementById('clear_button');
 
 
 function deleteTasks() {
-    taskList.removeChild(tasks);
-}
+    while (taskList.hasChildNodes()) {
+        taskList.removeChild(taskList.firstChild);
+    }
+};
 
 submitButton.addEventListener('click',createTask);
 clearButton.addEventListener('click',deleteTasks);
